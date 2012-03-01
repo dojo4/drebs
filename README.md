b426059dd80a13de05ce51665c20c4a1f18d309c
Garett Shulman <garett@dojo4.com>
2012-02-29 20:38:35 -0700

    DREBS 0.0.1

---
+++ README.md
@@ -0,0 +1,21 @@
+# DREBS (Disaster Recovery for Elastic Block Store)
+
+## About
+* DREBS is a tool for taking periodic snapshots of EBS volumes.
+* DREBS is designed to be run on the EC2 host which the EBS volumes to be snapshoted are attached.
+* DREBS supports configurable retention strategies.
+* DREBS supports configurable pre and post snapshot tasks such as dumping databases prior to snapshot for consistency.
+
+## Installation & Setup
+1. Clone the repo or install the gem
+1. Create a configuration file.  See example with: drebs test_config example_config
+1. Add Crontab entry: 0 * * * * drebs
+
+## Issues
+* State including config is cached in drebs_state.json.  This file will need to be deleted to get drebs to pick up new state.  Doing so will orphan current snapshots which will need to be deleted manually.
+
+## Todo
+* Tests!
+* Refactor using main with db for state and external config
+* Use Whenever gem for crontab setup
+* Arbitrary execution intervals (Snapshots every 5 minutes instead of every hour)
---
+++ lib/drebs.rb
@@ -0,0 +1,63 @@
+# -*- encoding : utf-8 -*-
+# built-ins
+#
+
+# DREBS libs
+#
+  module DREBS
+    Version = '0.0.1' unless defined?(Version)
+
+    def version
+      DREBS::Version
+    end
+
+    def dependencies
+      {
+        'right_aws' => [ 'right_aws' , ' >= 3.0.0 ' ] , 
+        'logger'    => [ 'logger'    , ' >= 1.2.8 ' ] , 
+        'main'      => [ 'main'      , ' >= 5.0.0 ' ] , 
+        'systemu'   => [ 'systemu'   , ' >= 2.4.2 ' ] , 
+        'json'      => [ 'json'      , ' >= 1.5.1 ' ] , 
+        }
+    end
+
+    def libdir(*args, &block)
+      @libdir ||= File.expand_path(__FILE__).sub(/\.rb$/,'')
+      args.empty? ? @libdir : File.join(@libdir, *args)
+    ensure
+      if block
+        begin
+          $LOAD_PATH.unshift(@libdir)
+          block.call()
+        ensure
+          $LOAD_PATH.shift()
+        end
+      end
+    end
+
+    def load(*libs)
+      libs = libs.join(' ').scan(/[^\s+]+/)
+      DREBS.libdir{ libs.each{|lib| Kernel.load(lib) } }
+    end
+
+    extend(DREBS)
+  end
+  Drebs = DREBS
+
+# gems
+#
+  begin
+    require 'rubygems'
+  rescue LoadError
+    nil
+  end
+
+  if defined?(gem)
+    DREBS.dependencies.each do |lib, dependency|
+      gem(*dependency)
+      require(lib)
+    end
+  end
+
+  DREBS.load %w[
+]
