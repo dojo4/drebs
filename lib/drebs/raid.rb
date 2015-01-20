6e8410595a51a24f97eb633553f1e7bb6fc8c64a
Dominik Zyla <dominik.zyla@gmail.com>
2015-01-20 18:43:05 +0000

    added raid.rb

---
+++ lib/drebs/raid.rb
@@ -0,0 +1,35 @@
+module Drebs
+  module Raid
+    def get_drives_from_raid(array, file='/proc/mdstat')
+      f     = File.read(file)
+      disks = Array.new()
+
+      # Here we parse the following file content from which we determine
+      # the list of decices:
+      #
+      # Personalities : [raid10]
+      # md126 : active raid10 xvdm[3] xvdl[2] xvdk[1] xvdj[0]
+      #       209583104 blocks super 1.2 512K chunks 2 near-copies [4/4] [UUUU]
+      #
+      # md127 : active raid10 xvdf[3] xvdg[2] xvdi[1] xvdh[0]
+      #       209583104 blocks super 1.2 512K chunks 2 near-copies [4/4] [UUUU]
+      #
+      # unused devices: <none>
+
+      f.each_line do |line|
+        if line =~ /^md\d+/
+          arr = line.split()
+          if arr[3] =~ /^raid\d+/
+            if arr[0] == array
+              disks << arr[4..-1].map{|drive| drive.split('[')[0] }
+            end
+          end
+        end
+      end
+
+      return disks
+    end
+
+    extend self
+  end
+end
