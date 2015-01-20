ace68c3708c872189f879ab904b920712169d3b8
Dominik Zyla <dominik.zyla@gmail.com>
2015-01-20 23:33:21 +0000

    rolled back test content.

--- test/unit/drebs/drebs_test.rb
+++ test/unit/drebs/drebs_test.rb
@@ -1,6 +1,6 @@
 require "./test/helper.rb"
 
-class DrebsMain < Minitest::Test
+class DrebsMain < Test::Unit::TestCase
 
   def test_backups_get_pruned
     TestContext.drebs_context() do |config, db, cloud, drebs|
